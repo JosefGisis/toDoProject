bcee1fd28869d325f3c0b323d095037102c00f8a
josef gisis <jsquared.gisis@gmail.com>
2023-12-07 17:19:26 -0500

    rando commit

--- collections.js
+++ collections.js
@@ -1,18 +1,34 @@
+class ToDo {
+    constructor (title, dueDate = null) {
+        this.title = title
+        this.dueDate = dueDate
+        this.creationDate = new Date().toLocaleString()
+        this.completed = false
+    }
+}
+
+
 class Collection {
     constructor(title, description = null) {
         this.title = title
         this.description = description
-    
-        this.displayProperties = function() {
-            for (let key in this)
-                console.log(`${key}: ${this[key]}`)}    
+        this.creationDate = new Date().toLocaleString()
+        this.toDos = []   
     }
+
+    addToDo(title, dueDate = null) {
+        const newToDo = new ToDo(title, dueDate)
+        this.toDos.push(newToDo)
+    }
+
 }
+
 
 class DefaultCollection extends Collection { 
     constructor() {
         super('Have You?', 'This is your default collection')
     }
 }
+
 
 export { Collection, DefaultCollection }
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { Collection, DefaultCollection } from './collections.js'
-import { ToDo } from './todos.js'
 
 function clearFields() {
     const collectionTitle = document.getElementById('collection-title')
@@ -9,10 +8,21 @@ function clearFields() {
     collectionDescription.value = ""
 }
 
+
+function checkTitleField() {
+    const collectionTitle = document.getElementById('collection-title')
+    const createButton = document.getElementById('create-button')
+
+    createButton.style.backgroundColor = (collectionTitle.value) ? 'rgb(14 165 233)' : 'rgb(7, 89, 133)'
+}
+
+
 function toggleCollectionForm() {
     const newCollectionForm = document.getElementById('new-collection-form')
     newCollectionForm.style.display = (newCollectionForm.style.display === 'none') ? 'block' : 'none'
+    if (newCollectionForm.style.display === 'block') checkTitleField()
 }
+
 
 function cancelCollection(e) {
     const newCollectionForm = document.getElementById('new-collection-form')
@@ -24,13 +34,33 @@ function cancelCollection(e) {
 
 const newCollectionButton = document.getElementById('new-collection-button')
 const cancelButton = document.getElementById('cancel-button')
-const submitButton = document.getElementById('create-button')
+const createButton = document.getElementById('create-button')
+const collectionTitle = document.getElementById('collection-title')
 
 newCollectionButton.addEventListener('click', toggleCollectionForm)
 cancelButton.addEventListener('click', cancelCollection)
+collectionTitle.addEventListener('input', checkTitleField)
 
-const defaultCollection = new DefaultCollection()
-console.log(defaultCollection.title, defaultCollection.description)
+/**
+ *
+**/
+const collections = []
+const defaultCollection = new DefaultCollection('Have You?', 'This is your default collection')
+const secondCollection = new Collection('Shopping list', 'this is my weekly shopping list')
+collections.push(defaultCollection, secondCollection)
+const collectionSection = document.getElementById('collection-section')
 
-const newToDO = new ToDo('get cheese', 'we are all out of cheese', 'tommorow')
-console.log(newToDO.title, newToDO.description, newToDO.dueDate)
+for (let collection of collections) {
+    let newCollectionHTML = `
+    <div class="collection-card">
+        <h3 class="text-2xl text-lime-600 | mb-5">${ collection.title }</h3>                 
+        <h4 style="font-size: 130%" class=" my-2">${ collection.description }<h4>
+        <p><i>Created ${ collection.creationDate }</i></p>
+        <input style="background-color: lightblue; width: 50%" type='text'>
+    </div>
+    `
+    const parser = new DOMParser()
+    const newCollection = parser.parseFromString(newCollectionHTML, 'text/html').body.firstChild
+    collectionSection.appendChild(newCollection)
+
+}
