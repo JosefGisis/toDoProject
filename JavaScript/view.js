48c3aeeca8dccb31185c37ceb9e865e376fbf3f2
josef gisis <jsquared.gisis@gmail.com>
2023-12-11 11:27:45 -0500

    added icons

--- JavaScript/view.js
+++ JavaScript/view.js
@@ -30,10 +30,16 @@ const toDoView = {
     const listSection = document.getElementById("list-section")
     for (let toDo of toDos) {
       const toDoHTML = `
-      <div class="flex flex-row content-center items-center | rounded-lg | bg-slate-800 | transition-all | p-3 mb-5 hover:bg-slate-600 hover:pl-7">
+      <div class="flex flex-row content-center items-center justify-between | rounded-lg | bg-slate-800 | transition-all | p-3 mb-5 hover:bg-slate-600 hover:pl-7">
         <div>
           <h3 class="rounded-lg | text-xl font-bold">${ toDo.title }</h3>
           <p class=""><i>Created: ${ toDo.creationDate }</i></p>
+        </div>
+        
+        <div aria-label="new todo list button" class="border-2 rounded-md">
+          <div id="new-list-button" class="peer | w-fit | p-3 rounded-md | bg-slate-700 hover:bg-slate-600 | transition-all">
+            <img src="/images/delete-icon.svg" width="15px" height="15px" alt="icon for new list button">         
+          </div>
         </div>
       </div>
       `
