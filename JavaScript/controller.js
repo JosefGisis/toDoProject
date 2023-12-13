1246225841212f02579c2c27f6d638b30b3b27e2
josef gisis <jsquared.gisis@gmail.com>
2023-12-13 10:57:03 -0500

    formatted documents

--- JavaScript/controller.js
+++ JavaScript/controller.js
@@ -3,96 +3,93 @@ import { changeListFormView, listView, toDoView, newListFormView } from './view.
 
 // retrieveData and saveData objects may be placed in model file
 const retrieveData = {
-    retrieveLists() {},
-    retrieveToDos() {}
-
+	retrieveLists() {},
+	retrieveToDos() {},
 }
 
 const saveData = {
-    saveLists() {},
-    saveToDos() {}
+	saveLists() {},
+	saveToDos() {},
 }
-
 
 // controller object initiates all objects and provides correspondence between model and view.
 const controller = {
-    init() {
-        if (model.lists[0]) model.currentList = model.lists[0]
-        
-        listView.init()
-        // toDoView.init()
-        changeListFormView.init()
-        changeListFormController.init()
-        newListFormController.init()
+	init() {
+		if (model.lists[0]) model.currentList = model.lists[0]
 
-    },
+		listView.init()
+		// toDoView.init()
+		changeListFormView.init()
+		changeListFormController.init()
+		newListFormController.init()
+	},
 
-    getToDos() {
-        return model.toDos
-    },
+	getToDos() {
+		return model.toDos
+	},
 
-    getCurrentList() {
-        return model.currentList
-    },
-    
-    getLists() {
-        return model.lists
-    }
+	getCurrentList() {
+		return model.currentList
+	},
+
+	getLists() {
+		return model.lists
+	},
 }
-
 
 // controls new list form (submission, cancellation, and list instantiation)
 const newListFormController = {
-    newListButton: document.getElementById("new-list-button"),
-    newListCancel: document.getElementById("new-list-cancel"),
-    newListSubmit: document.getElementById("new-list-submit"),
-    newListTitle: document.getElementById("new-list-title"),
-   
-    init() {
-        this.newListButton.addEventListener("click", newListFormView.toggleListForm.bind(newListFormView))
-        // cancelChange requires a new event listener because multiple functions cannot be invoked on the same line as they require binding to an external object.
-        this.newListButton.addEventListener("click", changeListFormView.cancelChange)
-        this.newListCancel.addEventListener("click", newListFormView.cancelList.bind(newListFormView))
-        this.newListTitle.addEventListener("input", newListFormView.checkTitleField.bind(newListFormView))
-        this.newListSubmit.addEventListener('click', this.newList)
-    },
-    
-    newList(e) {
-        e.preventDefault()
-        let newListTitle = document.getElementById("new-list-title")
-        let newListDescription = document.getElementById("new-list-description")
-        const newList = new List(newListTitle.value, newListDescription.value)
-        model.lists.push(newList)
-        // Fields need to be cleared to prevent submission spamming
-        newListTitle.value = ''
-        newListDescription.value = ''
-    }
-}
+	newListButton: document.getElementById('new-list-button'),
+	newListCancel: document.getElementById('new-list-cancel'),
+	newListSubmit: document.getElementById('new-list-submit'),
+	newListTitle: document.getElementById('new-list-title'),
 
+	init() {
+		this.newListButton.addEventListener('click', newListFormView.toggleListForm.bind(newListFormView))
+		// cancelChange requires a new event listener because multiple functions cannot be invoked on the same line as they require binding to an external object.
+		this.newListButton.addEventListener('click', changeListFormView.cancelChange)
+		this.newListCancel.addEventListener('click', newListFormView.cancelList.bind(newListFormView))
+		this.newListTitle.addEventListener('input', newListFormView.checkTitleField.bind(newListFormView))
+		this.newListSubmit.addEventListener('click', this.newList)
+	},
+
+	newList(e) {
+		e.preventDefault()
+		let newListTitle = document.getElementById('new-list-title')
+		let newListDescription = document.getElementById('new-list-description')
+		const newList = new List(newListTitle.value, newListDescription.value)
+		model.lists.push(newList)
+		// Fields need to be cleared to prevent submission spamming
+		newListTitle.value = ''
+		newListDescription.value = ''
+		changeListFormView.init()
+		console.log(model.lists)
+	},
+}
 
 // Controls list change form (list change submission/cancellation)
 const changeListFormController = {
-    changeListButton: document.getElementById('change-list-button'),
-    changeListCancel: document.getElementById('change-list-cancel'),
-    changeListSubmit: document.getElementById('change-list-submit'),
-    
-    init() {
-        this.changeListButton.addEventListener('click', changeListFormView.toggleChangeForm)
-        // cancelList requires a new event listener because multiple functions cannot be invoked on the same line as they require binding to an external object.
-        this.changeListButton.addEventListener('click', newListFormView.cancelList.bind(newListFormView))
-        this.changeListCancel.addEventListener('click', changeListFormView.cancelChange)
-        this.changeListSubmit.addEventListener('click', this.changeList)
-    },
+	changeListButton: document.getElementById('change-list-button'),
+	changeListCancel: document.getElementById('change-list-cancel'),
+	changeListSubmit: document.getElementById('change-list-submit'),
 
-    changeList(e) {
-        const listChangeMenu = document.getElementById("list-change-menu")
-        e.preventDefault()
-        for (let list of model.lists)
+	init() {
+		this.changeListButton.addEventListener('click', changeListFormView.toggleChangeForm)
+		// cancelList requires a new event listener because multiple functions cannot be invoked on the same line as they require binding to an external object.
+		this.changeListButton.addEventListener('click', newListFormView.cancelList.bind(newListFormView))
+		this.changeListCancel.addEventListener('click', changeListFormView.cancelChange)
+		this.changeListSubmit.addEventListener('click', this.changeList)
+	},
+
+	changeList(e) {
+		const listChangeMenu = document.getElementById('list-change-menu')
+		e.preventDefault()
+		for (let list of model.lists) 
             if (list.title === listChangeMenu.value) model.currentList = list
-        changeListFormView.cancelChange(e)
-        // Clear and reset list change drop-down menu
-        changeListFormView.init()
-    }
+		changeListFormView.cancelChange(e)
+		// Clear and reset list change drop-down menu
+		changeListFormView.init()
+	},
 }
 
 controller.init()
--- JavaScript/view.js
+++ JavaScript/view.js
@@ -1,100 +1,97 @@
 import { controller } from './controller.js'
 
-// change list form view handler. Responsible for handling form view, content, and list changes. 
+// change list form view handler. Responsible for handling form view, content, and list changes.
 const changeListFormView = {
   init() {
     const lists = controller.getLists()
     const currentList = controller.getCurrentList()
-    const listChangeMenu = document.getElementById("list-change-menu")
+    const listChangeMenu = document.getElementById('list-change-menu')
     // clear change drop-down menu, so lists are not duplicated
     listChangeMenu.innerHTML = ''
 
     for (let list of lists) {
       const option = document.createElement('option')
       option.textContent = list.title
-      // currently displayed list shows up as an option but is not selectable  
+      // currently displayed list shows up as an option but is not selectable
       if (list.title === currentList.title) {
         option.selected = option.disabled = true
-      }  
+      }
       listChangeMenu.appendChild(option)
     }
   },
 
   toggleChangeForm() {
-    const changeListForm = document.getElementById("change-list-form")
+    const changeListForm = document.getElementById('change-list-form')
     if (changeListForm.classList.contains('hidden')) changeListForm.classList.replace('hidden', 'block')
     else changeListForm.classList.replace('block', 'hidden')
   },
 
   cancelChange(e) {
-    const changeListForm = document.getElementById("change-list-form")
+    const changeListForm = document.getElementById('change-list-form')
     e.preventDefault()
     changeListForm.classList.replace('block', 'hidden')
-  }
+  },
 }
-
 
 // new list form view handler. Responsible for handling form view and new list submissions.
 const newListFormView = {
   clearFields() {
-    const newListTitle = document.getElementById("new-list-title")
-    const newListDescription = document.getElementById("new-list-description")
-    newListTitle.value = ""
-    newListDescription.value = ""
+    const newListTitle = document.getElementById('new-list-title')
+    const newListDescription = document.getElementById('new-list-description')
+    newListTitle.value = ''
+    newListDescription.value = ''
   },
-  
+
   checkTitleField() {
-    const newListTitle = document.getElementById("new-list-title")
-    const newListSubmit = document.getElementById("new-list-submit")
-    newListSubmit.style.backgroundColor = newListTitle.value ? "rgb(14 165 233)" : "rgb(7, 89, 133)"
+    const newListTitle = document.getElementById('new-list-title')
+    const newListSubmit = document.getElementById('new-list-submit')
+    newListSubmit.style.backgroundColor = newListTitle.value ? 'rgb(14 165 233)' : 'rgb(7, 89, 133)'
   },
-  
+
   toggleListForm() {
-    const newListForm = document.getElementById("new-list-form")
+    const newListForm = document.getElementById('new-list-form')
     if (newListForm.classList.contains('hidden')) newListForm.classList.replace('hidden', 'block')
     else newListForm.classList.replace('block', 'hidden')
-    // if form is visible check if title has been entered 
-    if (newListForm.style.display === "block") this.checkTitleField()
+    // if form is visible check if title has been entered
+    if (newListForm.style.display === 'block') this.checkTitleField()
   },
-  
+
   cancelList(e) {
-    const newListForm = document.getElementById("new-list-form")
+    const newListForm = document.getElementById('new-list-form')
     e.preventDefault()
     newListForm.classList.replace('block', 'hidden')
     this.clearFields()
-  }, 
+  },
 }
-
 
 const listView = {
   init() {
     const currentList = controller.getCurrentList()
-    const toDoSection = document.getElementById("todo-section")
-    const newToDoForm = document.getElementById("new-todo-form")
+    const toDoSection = document.getElementById('todo-section')
+    const newToDoForm = document.getElementById('new-todo-form')
     const currentListHTML = `
     <div>
-      <h3 class="w-fit rounded-lg | bg-sky-500 | text-4xl font-bold | p-3  mb-5">${ currentList.title }</h3>
-      <p class=""><i>Created: ${ currentList.creationDate }</i></p>
-      <h4 class="text-2xl font-semibold my-2">${ currentList.description }</h4>
+      <h3 class="w-fit rounded-lg | bg-sky-500 | text-4xl font-bold | p-3  mb-5">${currentList.title}</h3>
+      <p class=""><i>Created: ${currentList.creationDate}</i></p>
+      <h4 class="text-2xl font-semibold my-2">${currentList.description}</h4>
     </div>
     `
     const parser = new DOMParser()
-    const currentListNode = parser.parseFromString(currentListHTML, "text/html").body.firstChild
+    const currentListNode = parser.parseFromString(currentListHTML, 'text/html').body.firstChild
     toDoSection.insertBefore(currentListNode, newToDoForm)
-  }
+  },
 }
-
 
 const toDoView = {
   init() {
     const toDos = controller.getToDos()
-    const toDoSection = document.getElementById("todo-section")
+    const toDoSection = document.getElementById('todo-section')
     for (let toDo of toDos) {
       const toDoHTML = `
       <div class="flex flex-row content-center items-center justify-between | rounded-lg | bg-slate-800 | transition-all | p-3 mb-5 hover:bg-slate-600 hover:pl-7">
         <div>
-          <h3 class="rounded-lg | text-xl font-bold">${ toDo.title }</h3>
-          <p class=""><i>Created: ${ toDo.creationDate }</i></p>
+          <h3 class="rounded-lg | text-xl font-bold">${toDo.title}</h3>
+          <p class=""><i>Created: ${toDo.creationDate}</i></p>
         </div>
         
         <div aria-label="new todo list button" class="border-2 rounded-md">
@@ -105,11 +102,10 @@ const toDoView = {
       </div>
       `
       const parser = new DOMParser()
-      const newToDo = parser.parseFromString(toDoHTML, "text/html").body.firstChild
+      const newToDo = parser.parseFromString(toDoHTML, 'text/html').body.firstChild
       toDoSection.appendChild(newToDo)
     }
-  }
+  },
 }
-
 
 export { toDoView, listView, changeListFormView, newListFormView }
